31be66a3e2fece9d586e5ca9a4a23740e91fd4e5
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2021-06-26 13:10:39 +0200

    Update input styling to be clearer when focused

--- src/elements/Input/InputStyled.js
+++ src/elements/Input/InputStyled.js
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 import InputCss from './InputCss';
 
+import colors from 'tokens/colors';
+import radii from 'tokens/radii';
+
 // Extend the below as needed
 const InputStyled = styled.input`
   ${InputCss};
   border: 1px solid;
+
+  :focus {
+    outline: 1px solid ${colors.purple};
+    border-radius: ${radii.sharp};
+  }
 `;
 
 export default InputStyled;
