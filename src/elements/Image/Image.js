76718825d5d4e4333c83884d7130d4360127385a
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2021-07-04 11:46:50 +0200

    Update

--- src/elements/Image/Image.js
+++ src/elements/Image/Image.js
@@ -5,6 +5,24 @@ import ImageStyled from './ImageStyled';
 
 const Image = ({ url, height, hardCorners }) => <ImageStyled src={url} height={height} hardCorners={hardCorners} />;
 
-Image.propTypes = {};
+Image.propTypes = {
+  /**
+   * The URL for the image to use.
+   */
+  url: PropTypes.string.isRequired,
+  /**
+   * The height of the element in pixels. Defaults to "296px".
+   */
+  height: PropTypes.string,
+  /**
+   * Should this have hard (sharp) corners? Defaults to false (rounded corners).
+   */
+  hardCorners: PropTypes.bool
+};
+
+Image.defaultProps = {
+  height: "296px",
+  hardCorners: false
+};
 
 export default Image;
