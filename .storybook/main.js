11dc9812ea88febcd4c025bd02b40244b2833387
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2021-07-18 16:48:08 +0200

    Update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/docs.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|mdx)"
   ],
   "addons": [
+    {
+      name: "@storybook/addon-docs",
+      options: { transcludeMarkdown: true },
+    },
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ]
-}
+};
